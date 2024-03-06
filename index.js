7eb5ea0cb984f5c3fc7c4153861a35b4fa59b29d
Yasir-Raza-SCE <yasir1485.sce@gmail.com>
2024-03-06 10:47:28 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// let user = prompt("Enter the 1.v 2.s 3.t");
+
+// if (user == "v"){
+//     let s=prompt(`enter s`);
+//     let t=prompt(`enter t`);
+//     alert(`velocity is ${s/t}`);
+// }
+// else if (user == "s"){
+//     let v=prompt(`enter v`);
+//     let t=prompt(`enter t`);
+//     alert(`distance is ${v/t}`);
+// }
+// else if (user == "t"){
+//     let s=prompt(`enter s`);
+//     let v=prompt(`enter v`);
+//     alert(`time is ${s/v}`);
+// }
+// else {
+//     alert(`invalid`);
+// }
+
+
+// let email = prompt("Enter Your Email");
+// let pass = prompt("Enter Your Password");
+
+// if (email == "suffah" && pass == "123"){
+//     alert("Login Successfully")
+// }else if (email == "BMJ" && pass == "321"){
+//     alert("Login Successfully")
+// }
+// else {
+//     alert("error")
+// }
